70aa4110c0f2e5f16ad5d5aec8b0633bd05b3999
dza49 <dza49@asb9820u-e01.csil.sfu.ca>
2022-07-09 18:19:03 -0700

    tcp RTT

--- TcpClient.py
+++ TcpClient.py
@@ -7,35 +7,21 @@ serverPort = 50007
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect( (serverName, serverPort) )
 
-
+t1 = time.time()
 # print ("connected to: " + str(serverName) + str(serverPort) )
 
 
-sentence = input('Please enter(hello):')
+# sentence = input('Please enter(hello):')
+sentence = "hello"
 clientSocket.send(sentence.encode())
 modifiedSentence = clientSocket.recv(1024)
 print( "Message received from Server: " + modifiedSentence.decode())
+
+t2 = time.time()
+
+print("RTT for TCP is", t2-t1)
 
 clientSocket.close()
 
 
 
-def RTT(url):
- 
-    # time when the signal is sent
-    t1 = time.time()
- 
-    r = requests.get(url)
- 
-    # time when acknowledgement of signal
-    # is received
-    t2 = time.time()
- 
-    # total time taken
-    tim = str(t2-t1)
- 
-    print("Time in seconds :" + tim)
-
-url= 'http://localhost'
-
-RTT(url)
---
+++ UDPClient.py
@@ -0,0 +1,27 @@
+from socket import *
+import time
+import requests
+
+serverName = 'localhost'
+serverPort = 50007
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.connect( (serverName, serverPort) )
+
+t1 = time.time()
+# print ("connected to: " + str(serverName) + str(serverPort) )
+
+
+# sentence = input('Please enter(hello):')
+sentence = "hello"
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print( "Message received from Server: " + modifiedSentence.decode())
+
+t2 = time.time()
+
+print("RTT for TCP is", t2-t1)
+
+clientSocket.close()
+
+
+
